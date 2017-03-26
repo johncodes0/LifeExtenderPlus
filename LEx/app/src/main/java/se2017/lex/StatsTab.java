215b107c755d3909859ff479e6d1c6bc03e7867b
lkev747 <lkev747@gmail.com>
2017-03-26 19:48:07 -0400

    Restart
    Made all activities empty activities

---
+++ LEx/app/src/main/java/se2017/lex/StatsTab.java
@@ -0,0 +1,13 @@
+package se2017.lex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatsTab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats_tab);
+    }
+}
