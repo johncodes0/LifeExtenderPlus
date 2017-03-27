d2fc8f41815cfa0a743e5a72f2c4333526ed8f0d
lkev747 <lkev747@gmail.com>
2017-03-27 01:35:45 -0400

    Further Bugfixing

--- LEx/app/src/main/java/se2017/lex/GoalsTab.java
+++ LEx/app/src/main/java/se2017/lex/GoalsTab.java
@@ -17,7 +17,7 @@ public class GoalsTab extends AppCompatActivity {
     public static GoalObjects[] Goals = new GoalObjects[5];
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals_tab);
 
