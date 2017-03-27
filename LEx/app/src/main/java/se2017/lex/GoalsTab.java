3d9990ccc6f77ce1422b4a8b3c2dc141b053a2cd
lkev747 <lkev747@gmail.com>
2017-03-26 20:27:20 -0400

    Test1

--- LEx/app/src/main/java/se2017/lex/GoalsTab.java
+++ LEx/app/src/main/java/se2017/lex/GoalsTab.java
@@ -2,6 +2,8 @@ package se2017.lex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class GoalsTab extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class GoalsTab extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals_tab);
+    }
+
+    public void makeNewGoal(View v){
+        Intent newGoal = new Intent (this, AddingGoals.class);
+        startActivity(newGoal);
     }
 }
--- LEx/app/src/main/java/se2017/lex/HomeTab.java
+++ LEx/app/src/main/java/se2017/lex/HomeTab.java
@@ -2,6 +2,8 @@ package se2017.lex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class HomeTab extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeTab extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_tab);
+    }
+
+    public void gotoGoals(View v){
+        Intent toGoals = new Intent (this, GoalsTab.class);
+        startActivity(toGoals);
     }
 }
