13d2e57a23071c3223c584ada7c8c78fd018a02c
phoenixrises <huang.daniel.1996@gmail.com>
2017-05-02 13:56:08 -0400

    Comments

--- LEx/app/src/main/java/se2017/lex/TimeObject.java
+++ LEx/app/src/main/java/se2017/lex/TimeObject.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 /**
  * Created by huang on 4/24/2017.
- * Object for time that is stored in the database by the applicataion
+ * Object for time that is stored in the database
  */
 
 public class TimeObject {
